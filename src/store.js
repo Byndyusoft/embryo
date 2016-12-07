9c7b36fe297f68947d6b02f496058c30459cd097
sadcitizen <sadcitizen@yandex.ru>
2016-12-07 14:54:14 +0500

    do not run devtool on release

--- src/store.js
+++ src/store.js
@@ -2,10 +2,12 @@ import { applyMiddleware, createStore, compose } from 'redux';
 import thunk from 'redux-thunk';
 import reducer from './reducer';
 
-export default createStore(
-    reducer,
-    compose(
-        applyMiddleware(thunk),
-        window.devToolsExtension ? window.devToolsExtension() : f => f
-    )
-);
+let devTools = f => f;
+
+if (PROJECT_ENV !== 'release') {
+    if (window.devToolsExtension) {
+        devTools = window.devToolsExtension();
+    }
+}
+
+export default createStore(reducer, compose(applyMiddleware(thunk), devTools));
