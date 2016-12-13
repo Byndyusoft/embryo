19eecf77eaf4d7b56f100f7210886c7361a2d7e8
sadcitizen <sadcitizen@yandex.ru>
2016-12-13 16:54:36 +0500

    add blank for login form

--- src/features/auth/reducer.js
+++ src/features/auth/reducer.js
@@ -1,25 +1,36 @@
 import * as types from './constants/action-types';
 import { createReducer, nextState } from '../../utilities';
+import localStorageTokenKey from '../../config';
 
-const initialState = {
-    isPending: false,
+let user = {
     isAuthenticated: false,
     username: '',
-    role: '',
+    role: ''
+};
+
+const token = localStorage.getItem(localStorageTokenKey);
+
+if (token !== null) {
+
+}
+
+const initialState = {
+    ...user,
+    isPending: false,
     error: ''
 };
 
 const actions = {
     [types.LOGIN_REQUEST]: state => {
-        return nextState(state, { isPending: true });
+        return nextState(state, { isPending: true, isAuthenticated: false });
     },
 
     [types.LOGIN_SUCCESS]: (state, action) => {
-        return nextState(state, action.payload);
+        return nextState(state, { isPending: false, isAuthenticated: true, ...action.payload });
     },
 
     [types.LOGIN_FAILURE]: (state, action) => {
-        return nextState(state, action.payload);
+        return nextState(state, { isPending: false, isAuthenticated: false, error: action.error });
     },
 
     [types.LOGIN_LOGOUT]: () => {
