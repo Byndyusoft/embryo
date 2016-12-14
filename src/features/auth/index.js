b1ad11e09f65ca0b701095c69bc2420d4ef0a4a6
sadcitizen <sadcitizen@yandex.ru>
2016-12-14 12:49:31 +0500

    auth/login is container

--- src/features/auth/index.js
+++ src/features/auth/index.js
@@ -1,4 +1,4 @@
 /* eslint-disable import/prefer-default-export */
-export { default as Login } from './login';
+export { default as Login } from './containers/login';
 export { default as ensure } from './hocs/ensure';
 /* eslint-enable import/prefer-default-export */
